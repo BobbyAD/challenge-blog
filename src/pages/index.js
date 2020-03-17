98c23ade08dc66d0609b0f7b7743b23bff2ccfad
Robert Driskell <driskell.ba@gmail.com>
2020-03-16 20:42:06 -0500

    Messing with fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,24 @@ import React from "react"
 import SEO from "../components/seo"
 import Button from "../components/button"
 import Blog from "../components/blog"
+import indexStyles from "../styles/indexStyles"
+
+import "../styles/reset.css"
+import "../styles/fonts.css"
 
 import { ThemeProvider } from "react-jss"
 
 const IndexPage = () => {
     const theme = {}
     const siteTitle = "Bobby's Coding Blog"
+    const classes = indexStyles()
 
     return (
         <ThemeProvider theme={theme}>
-            <div>{`${siteTitle}`}</div>
-            <Blog />
+            <div className={classes.container}>
+                <div className={classes.title}>{`${siteTitle}`}</div>
+                <Blog />
+            </div>
         </ThemeProvider>
     )
 }
---
+++ src/styles/indexStyles.js
@@ -0,0 +1,7 @@
+import { createUseStyles } from "react-jss"
+
+export default createUseStyles({
+    container: {
+        fontFamily: `'Source Sans Pro', sans-serif`,
+    }
+})
