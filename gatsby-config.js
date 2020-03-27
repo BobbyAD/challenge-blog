c528617fedda6a9ddfd21ab5f90340c312b7fec1
Robert Driskell <driskell.ba@gmail.com>
2020-03-27 10:15:45 -0500

    gatsby-plugin-jss added

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
         },
     },
     plugins: [
+        `gatsby-plugin-jss`,
         `gatsby-plugin-netlify-cms`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
