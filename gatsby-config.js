86b8dfb03cf92ba4aff326b20d2aa88917ae3d07
Robert Driskell <driskell.ba@gmail.com>
2020-03-26 22:13:14 -0500

    Will this work with plugin-offline disabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
         `gatsby-plugin-netlify-cms`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-feed-mdx`,
         {
